22693c658af1b6592ce7d1cc708a1f330b615121
prgayake <prgayake100@gmail.com>
2022-10-11 00:52:49 +0530

    docker  install script added

--- nodenpm.sh
+++ nodenpm.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 sudo apt-get update
+sudo apt-get install curl
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# source ~/.bashrc
+# nvm install latest -y
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
---
+++ phpwebapp.sh
@@ -0,0 +1,7 @@
+#!bin/sh
+sudo apt-get update
+sudo apt-get install apache2 -y
+# install php server 
+sudo apt-get install php -y
+# install mysql server
+sudo apt-get install mysql-server -y
